0548a9064d12b9f8530774fe08715f72d9a083d8
Hector JS <miguel.lin123@gmail.com>
2022-03-22 21:53:04 -0600

    funcionalidad completa, datos por default

--- js/app.js
+++ js/app.js
@@ -1,11 +1,70 @@
+(function(){
+    console.log('Datos default');
+
+    const pokeNameInput = document.getElementById("pokeName");
+    let pokeName = pokeNameInput.value;
+    let nombrePokemon = document.getElementById('nombrePoke');
+    let idPokemon = document.getElementById('idPoke');
+    let tipoPoke = document.getElementById('tipo');
+    let statsPoke = document.getElementById('stats');
+    let movPoke = document.getElementById('mov');
+
+    
+    pokeName = pokeName.toLowerCase();
+
+    const url = `https://pokeapi.co/api/v2/pokemon/bulbasaur`;
+
+    fetch(url).then((res) => {
+        if (res.status != "200") {
+            console.log(res);
+            pokeImage("assets/pokemon-sad.gif")
+        }
+        else {
+            return res.json();
+        }
+    }).then((data) => {
+
+        if (data) {
+
+            console.log(data);
+
+            let aux;
+
+            let pokeImg = data.sprites.other.home.front_default;
+            let pokemonname = data.forms[0].name;
+            let pokemonabil = data.abilities;
+            let identificadorPokemon =  data.id;
+
+
+            pokeImage(pokeImg);
+            
+            idPokemon.innerHTML = "No: "+ identificadorPokemon;
+            nombrePokemon.innerHTML = pokemonname;
+            
+            tipoPoke.innerHTML = extraerTipo(data.types);
+
+            movPoke.innerHTML = extraerAbilidades(pokemonabil);
+
+            statsPoke.innerHTML = extraerstats(data.stats)
+
+            extraerstats(data.stats)
+        }
+    });
+    
+
+})();
 
 const fetchPokemon = () => {
 
     const pokeNameInput = document.getElementById("pokeName");
     let pokeName = pokeNameInput.value;
-    let nombreEli = document.getElementById('nombreChido');
+    let nombrePokemon = document.getElementById('nombrePoke');
+    let idPokemon = document.getElementById('idPoke');
+    let tipoPoke = document.getElementById('tipo');
+    let statsPoke = document.getElementById('stats');
+    let movPoke = document.getElementById('mov');
 
-    //nombreEli.innerHTML  = "Eliza"
+
 
     pokeName = pokeName.toLowerCase();
 
@@ -23,21 +82,28 @@ const fetchPokemon = () => {
 
         if (data) {
 
-            // console.log(data);
+            console.log(data);
 
-            let pokeImg = data.sprites.front_default;
+            let aux;
+
+            let pokeImg = data.sprites.other.home.front_default;
             let pokemonname = data.forms[0].name;
             let pokemonabil = data.abilities;
-
-            nombreEli.innerHTML = pokemonname;
-             console.log(pokemonname);
-
-             extraerAbil(pokemonabil)
+            let identificadorPokemon =  data.id;
 
 
             pokeImage(pokeImg);
+            
+            idPokemon.innerHTML = "No: "+ identificadorPokemon;
+            nombrePokemon.innerHTML = pokemonname;
+            
+            tipoPoke.innerHTML = extraerTipo(data.types);
 
-            // console.log(pokeImg);
+            movPoke.innerHTML = extraerAbilidades(pokemonabil);
+
+            statsPoke.innerHTML = extraerstats(data.stats)
+
+            extraerstats(data.stats)
         }
     });
 }
@@ -48,15 +114,54 @@ const pokeImage = (url) => {
     pokePhoto.src = url;
 }
 
-const extraerAbil = ( cajaDeAbilidades ) => {
-
-    console.log("Funcion que extrae abilidades");
-
-    // console.log(cajaDeAbilidades[0].ability.name);
+const extraerAbilidades = (cajaDeAbilidades) => {
+    let aux;
 
     for (let index = 0; index < cajaDeAbilidades.length; index++) {
         console.log(cajaDeAbilidades[index].ability.name);
-        
+
+        if (index === 0) {
+            aux = cajaDeAbilidades[index].ability.name+ " ";
+        } 
+        else {
+            aux += cajaDeAbilidades[index].ability.name+ " ";
+        }
+
     }
+    return aux;
+}
+const extraerTipo = (cajaDeTipos) => {
+    let aux;
+
+    for (let index = 0; index < cajaDeTipos.length; index++) {
+
+        if (index === 0) {
+            aux = cajaDeTipos[index].type.name + " "; 
+        }
+        else {
+            aux += cajaDeTipos[index].type.name + " "; 
+        }
+
+    }
+    return aux;
+
+}
+
+const extraerstats = (cajaDestats) => {
+
+    let aux;
+
+    for (let index = 0; index < cajaDestats.length; index++) {
+        // console.log(cajaDestats[index].stat.name);
+        if (index === 0) {
+            aux = cajaDestats[index].stat.name + ": " + cajaDestats[index].base_stat + " | " ; 
+        }
+        else {
+            aux += cajaDestats[index].stat.name + ": " + cajaDestats[index].base_stat + " | "; 
+        }
+
+    }
+
+    return aux;
 
 }
